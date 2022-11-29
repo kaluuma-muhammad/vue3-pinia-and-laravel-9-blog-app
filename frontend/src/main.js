eb3cd981fd3aeb89b6c73a9370beaf78c1a2b4eb
Meddy <kaluumamhd@gmail.com>
2022-11-29 07:46:54 +0300

    app

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,14 +1,12 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import piniaPluginPersistedstate from 'pinia-plugin-persistedstate'
-import { PiniaExtractPlugin } from "pinia-extract";
 import App from './App.vue'
 import router from './router'
 import '@/axios'
 
 const pinia = createPinia()
 pinia.use(piniaPluginPersistedstate)
-pinia.use(PiniaExtractPlugin);
 
 const app = createApp(App)
 
